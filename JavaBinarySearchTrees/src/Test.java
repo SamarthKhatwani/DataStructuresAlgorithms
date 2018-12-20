a640294bed51b2cf936ffad791d5c082fb603e24
Samarth Khatwani <samarthkhatwani47@gmail.com>
2018-12-20 02:50:04 -0800

    BST JAVA

---
+++ JavaBinarySearchTrees/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Implementation of Binary Search Tree in Java");
+	}
+
+}
